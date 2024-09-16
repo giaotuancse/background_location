92850568b6256d596c6eb1552a8f167b3b225841
Tuan Giao <giaotuan.cse@gmail.com>
2024-09-16 11:28:08 +0800

    update context

--- android/src/main/kotlin/com/almoullim/background_location/BackgroundLocationService.kt
+++ android/src/main/kotlin/com/almoullim/background_location/BackgroundLocationService.kt
@@ -198,22 +198,22 @@ class BackgroundLocationService: MethodChannel.MethodCallHandler, PluginRegistry
     private fun checkLocationPermissionIsGiven(): Boolean {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
             (ActivityCompat.checkSelfPermission(
-                this,
+                context!!,
                 Manifest.permission.ACCESS_FINE_LOCATION
             ) === PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
+                context!!,
                 Manifest.permission.ACCESS_COARSE_LOCATION
             ) === PackageManager.PERMISSION_GRANTED) && ActivityCompat.checkSelfPermission(
-                this,
+                context!!,
                 Manifest.permission.ACCESS_BACKGROUND_LOCATION
             ) === PackageManager.PERMISSION_GRANTED
         } else {
             ActivityCompat.checkSelfPermission(
-                this,
+                context!!,
                 Manifest.permission.ACCESS_FINE_LOCATION
             ) === PackageManager.PERMISSION_GRANTED &&
                     ActivityCompat.checkSelfPermission(
-                        this,
+                        context!!,
                         Manifest.permission.ACCESS_COARSE_LOCATION
                     ) === PackageManager.PERMISSION_GRANTED
         }
